b1e2808f3bb30fb7d402d4a06de4284145a7ba5d
Abdulxamidov-Sherzod <sherzodbekoffical@gmail.com>
2022-04-07 23:03:04 +0500

    Initial Commit

---
+++ Tic_Tac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic_Tac
+//
+//  Created by Sherzod on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
